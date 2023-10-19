8a6a542e53ee82edf2c7c2a483c424b4ea74e528
James Beston <james@beston.me>
2023-10-19 12:15:52 +0100

    Delete theme-switcherThemeSwitcher.cy.tsx

--- src/components/shared/theme-switcherThemeSwitcher.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ThemeSwitcher from './theme-switcher'
-
-describe('<ThemeSwitcher />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ThemeSwitcher />)
-  })
-})
